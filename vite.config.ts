8bc5d860cfe51d3637839a8d4f5d72c098513969
Fabrizio Sebastian Romano Arnao <97324608+RomanoFS@users.noreply.github.com>
2025-12-05 16:17:17 -0500

    red_local

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    host: '0.0.0.0', // Opcional: para ver en red local si lo necesitas
+    port: 5003,
+  }
+
 })
